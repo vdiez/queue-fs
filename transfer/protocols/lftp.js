4cbb400586f33be2be308c6359c88a942bc1a13c
vdiez <diez@outlook.com>
2017-08-25 15:33:17 +0200

    fixed ssh credentials for the lftp protocol

--- transfer/protocols/lftp.js
+++ transfer/protocols/lftp.js
@@ -11,8 +11,8 @@ module.exports = function(params) {
     return ssh({
         host: box.ip,
         cmd: "sudo mkdir -p '" + dirname + "'; sudo chown upop '" + dirname + "'; lftp -u " + server.lftp.user + "," + server.lftp.pass + " " + server.ip + " -p " + server.lftp.port + ' -e "set net:timeout 10; set net:max-retries 1; set xfer:clobber yes; pget -c -n ' + route.concurrency + " '" + sprintf(server.file, transfer).replace(/'/g, "\\'").replace(/"/g, "\\\"") + "' -o '" + tmp.replace(/'/g, "\\'").replace(/"/g, "\\\"") + '\'; bye" && mv -f "' + tmp.replace(/"/g, "\\\"") + '" "' + transfer.target.replace(/"/g, "\\\"") + '"',
-        username: route.username || server.username || config.username,
-        password: route.password || server.password || config.password,
+        username: route.username || server.username || config.default_username,
+        password: route.password || server.password || config.default_password,
         id: box.ip + " " + route_idx
     });
 };
