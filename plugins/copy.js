b4e8d1441da5249ce4b6a04c46a824df1d0e767c
vdiez <diez@outlook.com>
2018-04-13 16:16:01 +0200

    fixed copy: create paths and require wamp

--- plugins/copy.js
+++ plugins/copy.js
@@ -1,8 +1,9 @@
 let fs = require('fs-extra');
 let path = require('path');
 let sprintf = require('sprintf-js').sprintf;
+let wamp = require('simple_wamp');
 
-module.exports = function(actions) {
+module.exports = function(actions, config) {
     if (!actions.hasOwnProperty('copy')) {
         actions.copy = function (file, params) {
             return new Promise(function (resolve, reject) {
@@ -19,39 +20,47 @@ module.exports = function(actions) {
                         if (err) return reject();
                         let readStream = fs.createReadStream(source);
 
-                        if (params.tmp) {
-                            final = target;
-                            target = path.posix.join(path.dirname(target), ".tmp", path.basename(target));
-                        }
-                        let writeStream = fs.createWriteStream(target);
-                        writeStream.on('close', function () {
-                            if (params.tmp) {
-                                fs.move(target, final, {overwrite: true}, err => {
-                                    if (err) reject(err);
+                        Promise.resolve()
+                            .then(() => {
+                                if (params.tmp) {
+                                    final = target;
+                                    target = path.posix.join(path.dirname(target), ".tmp", path.basename(target));
+                                    return fs.ensureDir(path.dirname(target));
+                                }
+                            })
+                            .then(() => fs.ensureDir(path.dirname(final)))
+                            .then(() => {
+                                let writeStream = fs.createWriteStream(target);
+                                writeStream.on('close', function () {
+                                    if (params.tmp) {
+                                        fs.move(target, final, {overwrite: true}, err => {
+                                            if (err) reject(err);
+                                            else resolve();
+                                        });
+                                    }
                                     else resolve();
                                 });
-                            }
-                            else resolve();
-                        });
-                        writeStream.on('error', err => reject(err));
-                        readStream.on('error', err => reject(err));
-                        readStream.pipe(writeStream);
+                                writeStream.on('error', err => reject(err));
+                                readStream.on('error', err => reject(err));
+                                readStream.pipe(writeStream);
 
-                        let transferred = 0;
-                        let percentage = 0;
-                        let wamp_router = params.wamp_router || config.default_router;
-                        let wamp_realm = params.wamp_realm || config.default_realm;
-                        if (params.job_id && params.progress && wamp_router && wamp_realm) {
-                            readStream.on('data', function(buffer) {
-                                transferred += buffer.length;
+                                let transferred = 0;
+                                let percentage = 0;
+                                let wamp_router = params.wamp_router || config.default_router;
+                                let wamp_realm = params.wamp_realm || config.default_realm;
+                                if (params.job_id && params.progress && wamp_router && wamp_realm) {
+                                    readStream.on('data', function(buffer) {
+                                        transferred += buffer.length;
 
-                                let tmp = Math.round(transferred * 100 / stats.size);
-                                if (percentage != tmp) {
-                                    percentage = tmp;
-                                    wamp(wamp_router, wamp_realm, 'publish', [params.topic || 'task_progress', [params.job_id, file, {current: transferred, total: stats.size, percentage: percentage}]]);
+                                        let tmp = Math.round(transferred * 100 / stats.size);
+                                        if (percentage != tmp) {
+                                            percentage = tmp;
+                                            wamp(wamp_router, wamp_realm, 'publish', [params.topic || 'task_progress', [params.job_id, file, {current: transferred, total: stats.size, percentage: percentage}]]);
+                                        }
+                                    });
                                 }
-                            });
-                        }
+                            })
+                            .catch(err => reject(err));
                     });
 
                 }
