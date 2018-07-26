1bae4e6c8a4218d3697027214c8c2537b27876a0
vdiez <diez@outlook.com>
2018-07-26 18:46:47 +0200

    added thumbnail to api call

--- templates/perfect_memory.js
+++ templates/perfect_memory.js
@@ -5,7 +5,7 @@ module.exports = (params, config) => {
     let db;
     params.make_public = true;
     actions.push({action: "aws_s3", requisite: file => file.property !== "logging", critical:true, params: params});
-    actions.push({id: "ffprobe_metadata", requisite: file => file.property !== "logging", action: "local", critical:true, params: file => new Promise((resolve, reject) => {
+    actions.push({id: "ffprobe_metadata", requisite: file => file.property !== "logging" && file.property !== "thumbnail", action: "local", critical:true, params: file => new Promise((resolve, reject) => {
             if (db) return resolve();
             mongodb.MongoClient.connect(new mongodb.Server(config.db_host, config.db_port), (err, client) => {
                 if (err) return reject('MongoDB error while connecting: ', err);
@@ -28,13 +28,33 @@ module.exports = (params, config) => {
                 source_is_filename: true
             };
         })
+        .then(() => db.collection(config.db_files).findOne({clip_id: file.clip_id, property: "thumbnail", type: file.type}))
+        .then(result => {
+            if (result) file.results['thumbnail'] = result;
+        })
     });
-    actions.push({action: "rest_call", requisite: file => file.property !== "logging", critical: true, params: file => {
+    actions.push({action: "rest_call", requisite: file => file.property !== "logging" && file.property !== "thumbnail", critical: true, params: file => {
         let path = require('path');
         let sprintf = require('sprintf-js').sprintf;
         let target = params.target || './';
         target = sprintf(target, file);
         if (!params.target_is_filename) target = path.posix.join(target, file.filename);
+
+        let body = {
+            //"url": "https://s3." + params.region + ".amazonaws.com/" + params.bucket + "/" + file.filename,
+            "url": encodeURI("http://" + params.bucket + ".s3.amazonaws.com/" + path.posix.normalize(target)),
+            "assetName": file.asset_name,
+            "eventId": file.event_id,
+            "tcIn": file.results['ffprobe_metadata'].timecode,
+            "duration": file.results['ffprobe_metadata'].duration
+        };
+        if (file.results['thumbnail']) {
+            let thumb = params.target || './';
+            thumb = sprintf(target, file.results['thumbnail']);
+            if (!params.target_is_filename) thumb = path.posix.join(thumb, file.filename);
+            body.thumbnail = encodeURI("http://" + params.bucket + ".s3.amazonaws.com/" + path.posix.normalize(thumb));
+        }
+
         return {request: {
                 url: "https://exchange-manager-api.platform.labs.pm/v1/requests",
                     method: 'POST',
@@ -46,14 +66,7 @@ module.exports = (params, config) => {
                         "name": "ingest_video_asset",
                         "inputs": {
                         "video_parameters": {
-                            "value": new Buffer(JSON.stringify({
-                                //"url": "https://s3." + params.region + ".amazonaws.com/" + params.bucket + "/" + file.filename,
-                                "url": encodeURI("http://" + params.bucket + ".s3.amazonaws.com/" + path.posix.normalize(target)),
-                                "assetName": file.asset_name,
-                                "eventId": file.event_id,
-                                "tcIn": file.results['ffprobe_metadata'].timecode,
-                                "duration": file.results['ffprobe_metadata'].duration
-                            })).toString("base64"),
+                            "value": new Buffer(JSON.stringify()).toString("base64"),
                                 "type": "binary"
                         }
                     }
