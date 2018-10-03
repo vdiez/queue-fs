15a871c8f7196dce03b756b562cd3fe815b8974c
vdiez <diez@outlook.com>
2018-10-03 21:59:10 +0200

    added lftp put parser

---
+++ plugins/stream_parsers/lftp_put.js
@@ -0,0 +1,13 @@
+module.exports = {
+    parse(data) {
+        if (data instanceof Buffer) data = data.toString('utf8');
+        let match_progress = data.match(/(\d+) \((\d+)%\)/);
+        if (match_progress) {
+            this.data.current = Number(match_progress[1]);
+            if (Number(match_progress[2]) !== this.data.percentage) {
+                this.data.percentage = Number(match_progress[2]);
+                if (this.publish) this.publish(this.data);
+            }
+        }
+    }
+};
