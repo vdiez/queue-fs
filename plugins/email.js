71dad95884b518abb00ee2d54fbfc584a49ca7fa
vdiez <diez@outlook.com>
2019-03-01 12:21:59 +0100

    simplified email plugin

--- plugins/email.js
+++ plugins/email.js
@@ -1,16 +1,8 @@
 let nodemailer = require('nodemailer');
-let sprintf = require('sprintf-js').sprintf;
-let path = require('path');
 
 module.exports = (actions, config) => {
     if (!actions.hasOwnProperty('email')) {
         actions.email = (file, params) => {
-            if (!params || !params.recipient) throw "Missing parameters";
-            let source = file.dirname;
-            if (params && params.hasOwnProperty('source')) source = params.source;
-            source = sprintf(source, file);
-            if (!params.source_is_filename) source = path.posix.join(source, file.filename);
-
             let transporter = nodemailer.createTransport(params.smtp_server || config.default_smtp_server);
             let mailOptions = {
                 from: params.from || config.default_from,
